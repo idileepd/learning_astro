59b353c2aac5bb379344154c8dfe1f9e2f228e72
Dileep Nagendra <idileepd@gmail.com>
2025-04-05 22:41:03 +0530

    added cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare({
+    imageService: "cloudflare",
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.jsonc",
+      persist: {
+        path: "./.cache/wrangler/v3",
+      },
+    },
+  }),
+
+  output: "server",
 });
